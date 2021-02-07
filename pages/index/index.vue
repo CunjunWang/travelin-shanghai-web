ca23d8b4ab544fa81b08b3ce7b46f1761e5029c7
Cunjun Wang <duckwcj@gmail.com>
2021-02-07 16:59:34 +0800

    创建线路详情页; 添加图标

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,6 +1,5 @@
 <template>
   <view class="page">
-    <button @tap="stationsNear">附近</button>
     <view v-if="buses.length !== 0" class="info-container">
       <view class="title-container">
         <image src="../../static/bus-station-1.png" mode="widthFix" class="icon-big"></image>
@@ -10,12 +9,15 @@
         <view v-for="b in buses" class="station-info">
           <view class="station-distance">
             <text class="station-name">{{ b.stationName }}</text>
-            <text class="distance">{{ b.distance }}</text>
+            <view class="distance-container">
+              <image src="../../static/walk-1.png" mode="widthFix" class="distance-icon"></image>
+              <text class="distance">{{ b.distance }}</text>
+            </view>
           </view>
           <view class="lines">
             <image src="../../static/bus-1.png" mode="widthFix" class="icon"></image>
             <text class="desc">本站线路:</text>
-            <span class="bus-line" v-for="(l, i) in b.lines" :key="i" @tap="busDirectionDetail(l)">{{ l.name }}</span>
+            <span class="bus-line" v-for="l in b.lines" @tap="directionDetail(l, 'bus')">{{ l.name }}</span>
           </view>
         </view>
       </view>
@@ -29,12 +31,16 @@
         <view v-for="m in metros" class="station-info">
           <view class="station-distance">
             <text class="station-name">{{ m.stationName }}</text>
-            <text class="distance">{{ m.distance }}</text>
+            <view class="distance-container">
+              <image src="../../static/walk-1.png" mode="widthFix" class="distance-icon"></image>
+              <text class="distance">{{ m.distance }}</text>
+            </view>
           </view>
           <view class="lines">
             <image src="../../static/metro-1.png" mode="widthFix" class="icon"></image>
             <text class="desc">本站线路:</text>
-            <span class="metro-line" v-for="l in m.lines" :style="{backgroundColor: '#' + l.lineColor}">{{ l.name }}</span>
+            <span class="metro-line" v-for="l in m.lines" @tap="directionDetail(l, 'metro')"
+                  :style="{backgroundColor: '#' + l.lineColor}">{{ l.name }}</span>
           </view>
         </view>
       </view>
@@ -50,34 +56,33 @@ export default {
       metros: []
     }
   },
-  onLoad() {
+  onLoad: function () {
+    let that = this;
+    uni.getLocation({
+      type: "gps",
+      success: function (resp) {
+        let lat = resp.latitude;
+        let lon = resp.longitude;
+        let busUrl = `${that.url.busStationsNear}?curLat=${lat}&curLon=${lon}`;
+        let metroUrl = `${that.url.metroStationsNear}?curLat=${lat}&curLon=${lon}`;
+        that.ajax(busUrl, "GET", null, function (resp) {
+          that.buses = resp.data.data;
+        })
+        that.ajax(metroUrl, "GET", null, function (resp) {
+          console.log(resp)
+          that.metros = resp.data.data;
+        })
+      }
+    })
   },
   methods: {
-    stationsNear: function () {
-      let that = this;
-      uni.getLocation({
-        type: "gps",
-        success: function (resp) {
-          let lat = resp.latitude;
-          let lon = resp.longitude;
-          let busUrl = `${that.url.busStationsNear}?curLat=${lat}&curLon=${lon}`;
-          let metroUrl = `${that.url.metroStationsNear}?curLat=${lat}&curLon=${lon}`;
-          that.ajax(busUrl, "GET", null, function (resp) {
-            that.buses = resp.data.data;
-          })
-          that.ajax(metroUrl, "GET", null, function (resp) {
-            console.log(resp)
-            that.metros = resp.data.data;
-          })
-        }
-      })
-    },
-    busDirectionDetail: function (line) {
-      console.log(line);
+    directionDetail: function (line, type) {
+      console.log(line)
+      console.log(type)
       uni.navigateTo({
-        url: `../bus_direction_detail/bus_direction_detail?name=${line.name}`
+        url: `../line_direction_detail/line_direction_detail?name=${line.name}&type=${type}`
       });
-    }
+    },
   }
 }
 </script>
---
+++ pages/line_direction_detail/line_direction_detail.vue
@@ -0,0 +1,26 @@
+<template>
+  <view>
+    线路详情: {{ name }}-{{ type }}
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "",
+      type: ""
+    }
+  },
+  onLoad: function (data) {
+    let that = this;
+    that.name = data.name;
+    that.type = data.type;
+  },
+  methods: {}
+}
+</script>
+
+<style>
+
+</style>
