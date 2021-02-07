488f702c89064d2b4a85785b36a79ed7424e0089
Cunjun Wang <duckwcj@gmail.com>
2021-02-07 12:38:25 +0800

    添加首页模块, 添加首页基础样式

--- main.js
+++ main.js
@@ -9,3 +9,45 @@ const app = new Vue({
     ...App
 })
 app.$mount()
+
+
+// 全局接口链接
+let busBaseUrl = "http://localhost:9090/api/bus"
+let metroBaseUrl = "http://localhost:9091/api/metro"
+Vue.prototype.url = {
+    busStationsNear: busBaseUrl + "/query/stations/nearby",
+    metroStationsNear: metroBaseUrl + "/query/stations/nearby"
+}
+
+// 全局ajax方法
+Vue.prototype.ajax = function (url, method, data, fun) {
+    uni.request({
+        "url": url,
+        "method": method,
+        // "header": {
+        //     token: uni.getStorageSync("token")
+        // },
+        "data": data,
+        success: function (resp) {
+            fun(resp)
+            // if (resp.statusCode === 401) {
+            //     uni.redirectTo({
+            //         url: '../login/login'
+            //     });
+            // } else if (resp.statusCode === 200 && resp.data.code === 200) {
+            //     let data = resp.data;
+            //     if (data.hasOwnProperty("token")) {
+            //         console.log(resp.data);
+            //         let token = data.token;
+            //         uni.setStorageSync("token", token);
+            //     }
+            //     fun(resp);
+            // } else {
+            //     uni.showToast({
+            //         icon: "none",
+            //         title: resp.data
+            //     });
+            // }
+        }
+    });
+}
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,52 +1,97 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
-		</view>
-	</view>
+  <view class="page">
+    <button @tap="stationsNear">附近</button>
+    <view class="info-container">
+      <view class="title-container">
+        <image src="../../static/bus-station-1.png" mode="widthFix" class="icon-big"></image>
+        <text class="title">附近的公交站:</text>
+      </view>
+      <view v-if="buses" class="station-container">
+        <view v-for="b in buses" class="station-info">
+          <view class="station-distance">
+            <text class="station-name">{{ b.stationName }}</text>
+            <text class="distance">{{ b.distance }}</text>
+          </view>
+          <view class="lines">
+            <image src="../../static/bus-1.png" mode="widthFix" class="icon"></image>
+            <text class="desc">本站线路:</text>
+            <span class="line" v-for="l in b.lines">{{ l + ", " }}</span>
+          </view>
+        </view>
+      </view>
+    </view>
+    <view class="info-container">
+      <view class="title-container">
+        <image src="../../static/sh-metro-1.png" mode="widthFix" class="icon-big"></image>
+        <text class="title">附近的地铁站:</text>
+      </view>
+      <view v-if="metros" class="station-container">
+        <view v-for="m in metros" class="station-info">
+          <view class="station-distance">
+            <text class="station-name">{{ m.stationName }}</text>
+            <text class="distance">{{ m.distance }}</text>
+          </view>
+          <view class="lines">
+            <image src="../../static/metro-1.png" mode="widthFix" class="icon"></image>
+            <text class="desc">本站线路:</text>
+            <span class="line" v-for="l in m.lines">{{ l + ", " }}</span>
+          </view>
+        </view>
+      </view>
+    </view>
+  </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
-
-		},
-		methods: {
-
-		}
-	}
+export default {
+  data() {
+    return {
+      buses: [
+        {
+          stationName: "广粤路丰镇路", distance: "55.8 米",
+          lines: ["21路-上行", "21路-下行"]
+        },
+        {
+          stationName: "凉城路丰镇路", distance: "125.5 米",
+          lines: ["874路-上行", "875路-下行", "745路-上行", "132路-下行"]
+        },
+      ],
+      metros: [
+        {
+          stationName: "宝山路", distance: "1333 米",
+          lines: ["3号线", "4号线"]
+        }
+      ],
+    }
+  },
+  onLoad() {
+  },
+  methods: {
+    stationsNear: function () {
+      let that = this;
+      uni.getLocation({
+        type: "gps",
+        success: function (resp) {
+          console.log(resp);
+          let lat = resp.latitude;
+          let lon = resp.longitude;
+          let busUrl = `${that.url.busStationsNear}?curLat=${lat}&curLon=${lon}`;
+          let metroUrl = `${that.url.metroStationsNear}?curLat=${lat}&curLon=${lon}`;
+          that.ajax(busUrl, "GET", null, function (resp) {
+            console.log(resp);
+            that.buses = resp.data.data;
+          })
+          that.ajax(metroUrl, "GET", null, function (resp) {
+            console.log(resp);
+            that.metros = resp.data.data;
+          })
+        }
+      })
+    }
+  }
+}
 </script>
 
-<style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+<style lang="less">
+@import url("index.less");
 </style>
