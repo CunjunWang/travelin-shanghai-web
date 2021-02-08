3f9fe791e23b314062ba6c5a9cf04010a3819315
Cunjun Wang <duckwcj@gmail.com>
2021-02-08 12:16:40 +0800

    线路详情页样式设计

--- pages/line_direction_detail/line_direction_detail.vue
+++ pages/line_direction_detail/line_direction_detail.vue
@@ -1,5 +1,5 @@
 <template>
-  <view>
+  <view class="page">
     TODO: 引入地图在地图上展示线路
     <view class="title">
       <text class="line-name">{{ name }}</text>
@@ -7,10 +7,29 @@
     </view>
     <view class="direction-container">
       <view class="direction-info" v-for="d in directions">
-        {{ name }}-开往: {{ d.dest }}
-        <view>
-          <text>首班车: {{ d.first }}</text>
-          <text>末班车: {{ d.last }}</text>
+        <view class="line-and-direction">
+          <text class="line-name">{{ name }}</text>
+          <text class="line-direction">{{ d.direction }} 方向</text>
+        </view>
+        <view class="direction-detail">
+          <view class="row time-row">
+            <view>
+              <span class="small-text first">首</span>
+              <text class="small-content">{{ d.first }}</text>
+            </view>
+            <view>
+              <span class="small-text last">末</span>
+              <text class="small-content">{{ d.last }}</text>
+            </view>
+          </view>
+          <view class="row station-row">
+            <span class="small-text origin">起</span>
+            <text class="small-content">{{ d.origin }}</text>
+          </view>
+          <view class="row station-row">
+            <span class="small-text dest">终</span>
+            <text class="small-content">{{ d.dest }}</text>
+          </view>
         </view>
       </view>
     </view>
@@ -21,8 +40,23 @@
 export default {
   data() {
     return {
+      // name: "21路",
       name: "",
       directions: []
+        // {
+        //   direction: "下行",
+        //   origin: "胶州路愚园路",
+        //   dest: "广粤路丰镇路",
+        //   first: "05:30",
+        //   last: "22:50"
+        // },
+        // {
+        //   direction: "上行",
+        //   origin: "广粤路丰镇路",
+        //   dest: "胶州路愚园路",
+        //   first: "05:40",
+        //   last: "23:10"
+        // }
     }
   },
   onLoad: function (data) {
@@ -38,12 +72,13 @@ export default {
       url = that.url.metroLineDirectionTime;
     url = url + name
     that.ajax(url, "GET", null, function (resp) {
+      console.log(resp.data.data.data);
       let dir = [];
       for (let ldt of resp.data.data.data) {
         let d = {
           direction: ldt.direction,
           origin: ldt.origin,
-          dest: ldt.origin
+          dest: ldt.dest,
         }
         if (type === "bus") {
           d.first = ldt.firstBus;
